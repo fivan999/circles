66e9d9dcc61f8f6db94596f20b992483eaf41ac6
fivan999 <fivan999@yandex.ru>
2022-11-03 16:55:14 +0300

    task 3

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
 from random import randint
-
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QWidget, QApplication
-from PyQt5 import uic
 import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class Window(QWidget):
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(400, 300)
+        self.paint_button = QtWidgets.QPushButton(Form)
+        self.paint_button.setGeometry(QtCore.QRect(154, 270, 81, 23))
+        self.paint_button.setObjectName("paint_button")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Круги"))
+        self.paint_button.setText(_translate("Form", "Сделать круг"))
+
+
+class Window(QWidget, Ui_Form):
     def __init__(self):
         super().__init__()
-        uic.loadUi("UI.ui", self)
+        self.setupUi(self)
         self.setWindowTitle("Круги")
         self.paint_button.clicked.connect(self.paint)
         self.do_paint = False
@@ -29,7 +45,7 @@ class Window(QWidget):
         center_y = randint(50, 350)
         center_x = randint(50, 250)
         size = randint(50, 150)
-        colors = [255, 237, 0]
+        colors = [randint(0, 256), randint(0, 256), randint(0, 256)]
         color = QColor(*colors)
         self.qp.setBrush(color)
         self.qp.drawEllipse(center_x - size // 2,
